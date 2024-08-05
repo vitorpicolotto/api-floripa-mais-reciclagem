80cac0706eaa82572dd29700dc885d4b1e372b7a
vitorpicolotto <picolottovitor@gmail.com>
2024-08-04 21:55:56 -0300

    Criação do middleware de validação de token

---
+++ src/middlewares/validaToken.js
@@ -0,0 +1,29 @@
+const { verify } = require('jsonwebtoken')
+
+function validaToken(request, response, next) {
+    try {
+        const token = request.headers.authorization
+      
+        if (!token) {
+            return response.status(400).json({mensagem: 'Token não anexado'})
+        }
+
+        const jwt = token.split(" ")
+    
+        const resultado = verify(jwt[1], process.env.SECRET_JWT)
+        
+        request.usuarioId = resultado.id
+
+        next()
+    } catch (error) {
+        console.log(error)
+        if(error.message === "jwt malformed" || error.message === "jwt expired") {
+            response.status(401).json({mensagem: 'O Token é inválido ou já expirou'})
+        } else {
+            response.status(500).json({mensagem: 'Falha na requisição'})
+        }
+    }
+
+}
+
+module.exports = validaToken
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 // const swaggerUi = require('swagger-ui-express')
 // const swaggerDocument = require('./doc.swagger.json')
+const validaToken = require('../middlewares/validaToken')
 const usuariosRoutes = require('./usuarios.routes')
 const LoginController = require('../controllers/LoginController')
 const locaisRoutes = require('./locais.routes')
@@ -13,6 +14,6 @@ const routes = new Router()
 
 routes.use('/usuarios', usuariosRoutes) //rota pública
 routes.post('/login', LoginController.login)
-routes.use('/local', locaisRoutes) //pública -> privada (próximo passo)
+routes.use('/local', validaToken, locaisRoutes) //rota privada
 
 module.exports = routes
